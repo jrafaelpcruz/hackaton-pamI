0a42b62e4d5f2d947b64a2393c51609caf516fe2
jrafaelpcruz <rafaeldemizar@gmail.com>
2022-06-14 14:46:38 -0300

    estrutura da landing page trabalhada

--- index.php
+++ index.php
@@ -18,8 +18,63 @@
  <main>
 	<div class="spotlight">
 		<img src="img/woman_sunglasses.jpg"/>
-		<p><a href="#">Conteudo em spotlight!!</a></p>
+		<p><a href="#">Lorem ipsum dolor sit amet consectetur.</a></p>
 	</div>
+
+  <div class="container">
+    <div class="coluna-conteudo">
+      <div class="conteudo">
+        <div><img src="img/music.jpg" /></div>
+        <div>
+          <a href="#">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</a>
+          <div class="conteudo-atributos">
+            <ul>
+              <li>Conteudo-Tag</li>
+              <li>Data</li>
+            </ul>
+          <div class="conteudo-atributos-comentarios">
+            0
+          </div>
+        </div>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum, tenetur. Suscipit ullam libero autem consectetur doloribus debitis, quo delectus asperiores rem harum magnam quasi ut, illum temporibus laudantium laboriosam recusandae architecto illo nemo, pariatur vel! Magnam alias minima commodi autem velit. Incidunt blanditiis accusamus ipsam non quos quidem fugit et?</p>
+        </div>
+      </div>
+      <div class="conteudo">
+        <div>
+          <img src="img/column_woman_glasses.jpg" />
+        </div>
+        <div>
+          <a href="#">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, autem.n</a>
+          <div class="conteudo-atributos">
+            <ul>
+              <li>Conteudo-Tag</li>
+              <li>Data</li>
+            </ul>
+          <div class="conteudo-atributos-comentarios">
+            0
+          </div>
+        </div>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum, tenetur. Suscipit ullam libero autem consectetur doloribus debitis, quo delectus asperiores rem harum magnam quasi ut, illum temporibus laudantium laboriosam recusandae architecto illo nemo, pariatur vel! Magnam alias minima commodi autem velit. Incidunt blanditiis accusamus ipsam non quos quidem fugit et?</p>
+        </div>
+      </div>
+    </div>
+    <div class="conteudo-left">
+      <h1>Mais Comentados</h1>
+      <a href="#">Lorem ipsum dolor sit amet.</a>
+      <a href="#">Lorem, ipsum dolor.</a>
+      <h1>Mais Recentes</h1>
+      <a href="#">Lorem ipsum dolor sit amet.</a>
+      <a href="#">Lorem, ipsum dolor.</a>
+      <h1>Nossa Comunidade</h1>
+      <a href="#">Lorem ipsum dolor sit amet.</a>
+      <a href="#">Lorem, ipsum dolor.</a>      
+      <h1>Nos apoie</h1>
+      <div class="conteudo-left-apoie">
+        <img src="img/hands.jpg" />
+        <a href="#">Sua ajuda mantem nosso site vivo e independente</a>
+      </div>
+    </div>
+  </div>
  </main>
  
  <footer><?php include 'footer.php'?></footer>
