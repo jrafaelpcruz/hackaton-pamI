58f557d2bb88218c9c191c14d5323474851c9579
jrafaelpcruz <rafaeldemizar@gmail.com>
2022-06-14 10:42:26 -0300

    trabalho inicial

---
+++ header.php
@@ -0,0 +1,23 @@
+<div class="header">
+  <div>
+    <h1>Placeholder</h1>
+    <div class="topquote">
+      <p>"Buy my glasses."<p>
+      <p>-Woman in the picture</p>
+    </div>
+  </div>
+  <nav class="titlenav">
+    <ul class="topnav">
+      <li><a href="index.php">Home</a></li>
+      <li><a href="#">Contato</a></li>
+    </ul>
+  </nav>
+</div>
+<div class="compactnav">
+  <nav>
+    <ul class="topnav">
+      <li><a href="index.php">Home</a></li>
+      <li><a href="#">Contato</a></li>
+    </ul>
+  </nav>
+</div>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ptbr">
+  <link rel="stylesheet" href="styles/styles.css">
+  <!--google fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Crimson+Text:ital@1&family=Damion&display=swap" rel="stylesheet">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Placeholder Title</title>
+</head>
+<body>
+
+ <header><?php include 'header.php'?></header>
+	
+ <main>
+	<div class="spotlight">
+		<img src="img/woman_sunglasses.jpg"/>
+		<p><a href="#">Conteudo em spotlight!!</a></p>
+	</div>
+ </main>
+ 
+ <footer><?php include 'footer.php'?></footer>
+
+</body>
+</html>
